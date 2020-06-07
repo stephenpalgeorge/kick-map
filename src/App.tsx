43b3ba77692ba23ddba9fea4348c9fc486880c35
stephenpalgeorge <stephenpalgeorge@gmail.com>
2020-06-07 20:56:55 +0100

    animate map on polygon click

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import './App.scss';
 
 import { LONDON_BOROUGHS } from './_data';
 import { Card, PolygonMap } from './components';
+import { LatLngTuple } from 'leaflet';
 
 const App = () => {
+  const [mapCenter, setMapCenter] = useState<LatLngTuple>([51.505, -0.09]);
+  const [mapZoom, setMapZoom] = useState<number>(10);
   const [dataKey, setDataKey] = useState<string>('');
 
   return (
     <div className="App">
       <PolygonMap
         highlightColor="#D91F2D"
-        mapCenter={[51.505, -0.09]}
+        mapCenter={ mapCenter }
         mapData={ LONDON_BOROUGHS }
-        mapZoom={10}
+        mapZoom={ mapZoom }
         setDataKey={ setDataKey }
+        setMapCenter={ setMapCenter }
+        setMapZoom={ setMapZoom }
         tilesUrl="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
       />
       <Card activeDataKey={dataKey} />
--- src/components/02-molecules/PolygonMap/index.tsx
+++ src/components/02-molecules/PolygonMap/index.tsx
@@ -8,6 +8,8 @@ interface IPolygonMapProps {
   mapData: any[],
   mapZoom?: number,
   setDataKey: Dispatch<SetStateAction<string>>,
+  setMapCenter: Dispatch<SetStateAction<LatLngTuple>>,
+  setMapZoom: Dispatch<SetStateAction<number>>,
   tilesUrl: string
 }
 
@@ -17,6 +19,8 @@ const PolygonMap : React.FC<IPolygonMapProps> = ({
   mapData,
   mapZoom,
   setDataKey,
+  setMapCenter,
+  setMapZoom,
   tilesUrl
 } : IPolygonMapProps) => {
   const [label, setLabel] = useState<string>('');
@@ -26,7 +30,13 @@ const PolygonMap : React.FC<IPolygonMapProps> = ({
       <h2 className={`polygon-map__label ${label.length > 0 ? 'has-value' : ''}`}>
         { label }
       </h2>
-      <Map center={mapCenter} zoom={mapZoom}>
+      <Map
+        animate={ true }
+        center={ mapCenter }
+        zoom={ mapZoom }
+        viewport={{ center: mapCenter, zoom: mapZoom }}
+        scrollWheelZoom={ false }
+      >
         <GeoJSON
           data={ mapData }
           style={() => {
@@ -39,14 +49,18 @@ const PolygonMap : React.FC<IPolygonMapProps> = ({
           onEachFeature={(feature, layer: Path) => {
             layer.on('mouseover', () => {
               setLabel(feature.properties.name);
-              return layer.setStyle({ fillOpacity: 1 })
+              return layer.setStyle({ fillOpacity: 1 });
             });
             layer.on('mouseout', () => {
               setLabel('');
-              return layer.setStyle({ fillOpacity: .4 })
+              return layer.setStyle({ fillOpacity: .4 });
             });
             layer.on({
-              click: () => setDataKey(feature.properties.name)
+              click: () => {
+                setDataKey(feature.properties.name);
+                setMapCenter([51.205, -0.09]);
+                setMapZoom(9);
+              }
             });
           }}
         />
@@ -58,8 +72,6 @@ const PolygonMap : React.FC<IPolygonMapProps> = ({
 
 PolygonMap.defaultProps = {
   highlightColor: '#000000',
-  mapCenter: [51.505, -0.09],
-  mapZoom: 8
 }
 
 export default PolygonMap;
